8139669afc7ba202b7a3c5bd1b60c0a1290654ff
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 <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>
2026-08-03 04:25:05 +0300

    added product details

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("shop/", views.shop, name="shop"),
-    # path("products/<int:product_id>/", views.product_detail, name="product_detail"),
+    path("products/<slug:slug>/", views.product_detail, name="product_detail"),
     path("contact/", views.contact, name="contact"),
     
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from products.models import Product
+from django.shortcuts import get_object_or_404, render
+from products.models import Category, Product
+from django.db.models import Q
 
 def home(request):
     products = Product.objects.filter(
@@ -15,12 +16,47 @@ def home(request):
 
 def shop(request):
     products = Product.objects.filter(is_active=True)
+    categories = Category.objects.filter(is_active=True)
+    query = request.GET.get("q")
+    category = request.GET.get("category")
+
+    if query:
+        products = products.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query)|
+            Q(brand__name__icontains=query)
+        )
+
+    if category:
+        products = products.filter(category_id=category)
 
     context = {
         "products": products,
+        "categories": categories,
     }
 
     return render(request, "core/shop.html", context)
 
 def contact(request):
     return render(request, "core/contact.html")
+
+def product_detail(request, slug):
+    product = get_object_or_404(
+        Product.objects.select_related(
+            "category",
+            "brand",
+            "store"
+
+            ).prefetch_related(
+                "images",
+                "variants"
+            ),
+        slug=slug,
+        is_active=True
+    )
+
+    context = {
+        "product": product,
+    }
+
+    return render(request, "core/product_detail.html", context)
